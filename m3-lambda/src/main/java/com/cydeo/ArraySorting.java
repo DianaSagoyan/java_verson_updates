8d78bbfcef2efb2f890080ea270e6e5117eadd95
DianaSagoyan <dianasagoyan@yahoo.com>
2022-08-17 08:16:11 -0500

    Practicing lambda, added lombok

--- m3-lambda/src/main/java/com/cydeo/ArraySorting.java
+++ m3-lambda/src/main/java/com/cydeo/ArraySorting.java
@@ -1,4 +1,16 @@
 package com.cydeo;
 
 public class ArraySorting {
+    public static void main(String[] args) {
+        ArraySorting as = new ArraySorting();
+        QuickSort qs = new QuickSort();
+        BubbleSort bs = new BubbleSort();
+
+        qs.sort();
+        bs.sort();
+    }
+
+    private static void sort(Sorting sorting){
+        sorting.sort();
+    }
 }
