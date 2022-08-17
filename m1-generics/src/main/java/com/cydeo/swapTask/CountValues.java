f8b4006a8546ac3e57f98abf043ab6643ef0f381
DianaSagoyan <dianasagoyan@yahoo.com>
2022-08-16 21:59:45 -0500

    Practicing generics, new class CountValues

---
+++ m1-generics/src/main/java/com/cydeo/swapTask/CountValues.java
@@ -0,0 +1,5 @@
+package com.cydeo.swapTask;
+
+public class CountValues {
+    
+}
--- m1-generics/src/main/java/com/cydeo/swapTask/SwapValues.java
+++ m1-generics/src/main/java/com/cydeo/swapTask/SwapValues.java
@@ -1,7 +1,17 @@
 package com.cydeo.swapTask;
 
+import java.util.Arrays;
+
 public class SwapValues {
     public static void main(String[] args) {
-        
+        Integer[] a = {1, 2, 3, 4};
+        swap(a, 0, 2);
+        System.out.println(Arrays.toString(a));
+    }
+
+    private static <T> void swap(T[]a, int x, int y){
+        T temp = a[x];
+        a[x] = a[y];
+        a[y] = temp;
     }
 }
