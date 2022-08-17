3d975243a229532c2a10947a8a4bfc59f60830a0
DianaSagoyan <dianasagoyan@yahoo.com>
2022-08-16 20:23:11 -0500

    Created 3 new classes extending player class

---
+++ m1-generics/src/main/java/com/cydeo/BaseballPlayer.java
@@ -0,0 +1,7 @@
+package com.cydeo;
+
+public class BaseballPlayer extends Player{
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+}
--- m1-generics/src/main/java/com/cydeo/Player.java
+++ m1-generics/src/main/java/com/cydeo/Player.java
@@ -1,4 +1,17 @@
 package com.cydeo;
 
-public class Player {
+public abstract class Player {
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
