24270d75193f82cf0788d987d3621565701d212b
DianaSagoyan <dianasagoyan@yahoo.com>
2022-08-21 15:19:42 -0500

    Practicing Collectors

--- m6-stream/src/main/java/com/cydeo/CollectorsDemo.java
+++ m6-stream/src/main/java/com/cydeo/CollectorsDemo.java
@@ -3,6 +3,7 @@ package com.cydeo;
 
 import com.cydeo.task.Dish;
 import com.cydeo.task.DishData;
+import com.cydeo.task.Type;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -61,5 +62,35 @@ public class CollectorsDemo {
                 .filter(x -> x % 2 == 0)
                 .collect(Collectors.counting());
         System.out.println(evenCount);
+
+        System.out.println("-----------------------------------");
+
+        //averagingInt(ToIntFunction): returns the average of the integers passed values
+        Double calorieAverage = DishData.getAll().stream()
+                .collect(Collectors.averagingInt(Dish::getCalories));
+        System.out.println(calorieAverage);
+
+        System.out.println("-----------------------------------");
+
+        //joining(): is used to join various elements of a character or string array into a single object
+        List<String> courses = Arrays.asList("Java", "JavaScript", "TypeScript");
+        String result = courses.stream()
+                .collect(Collectors.joining(", "));
+        System.out.println(result);
+
+
+        System.out.println("-----------------------------------");
+
+        //partitioningBy(): is used to partition a stream of object (or a set of elements) based oon a given predicate
+        Map<Boolean, List<Dish>> map = DishData.getAll().stream()
+                .collect(Collectors.partitioningBy(Dish::isVegetarian));
+        System.out.println(map);
+
+        System.out.println("-----------------------------------");
+
+        //groupingBy(): is used to grouping objects by some property and storing results in a Map instance
+        Map<Type, List<Dish>> dishType = DishData.getAll().stream()
+                .collect(Collectors.groupingBy(Dish::getType));
+        System.out.println(dishType);
     }
 }
