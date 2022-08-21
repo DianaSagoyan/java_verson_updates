a343cdcdd3d8da25e49b6316dddb73f3910e4cfb
DianaSagoyan <dianasagoyan@yahoo.com>
2022-08-21 12:43:09 -0500

    Practicing terminal operations

--- m6-stream/src/main/java/com/cydeo/task/FindingMatching.java
+++ m6-stream/src/main/java/com/cydeo/task/FindingMatching.java
@@ -1,6 +1,10 @@
 package com.cydeo.task;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 public class FindingMatching {
     public static void main(String[] args) {
@@ -35,5 +39,32 @@ public class FindingMatching {
         Optional<Dish> dish2 = DishData.getAll().stream().filter(Dish::isVegetarian).findFirst();
         System.out.println(dish2);
         System.out.println(dish2.get());
+
+        System.out.println("--------------------------------------");
+
+        //FIND FIRST
+        System.out.println(IntStream.range(0, 100).parallel().findAny());
+        System.out.println(IntStream.range(0, 100).parallel().findFirst());
+
+        System.out.println("--------------------------------------");
+
+        List<String> list1 = Arrays.asList("Jhonny","David","Jack","Duke","Jill","Dany","Julia","Jenish","Divya");
+        List<String> list2 = Arrays.asList("Jhonny","David","Jack","Duke","Jill","Dany","Julia","Jenish","Divya");
+
+        Optional<String> findFirst = list1.parallelStream().filter(s -> s.startsWith("D")).findFirst();
+        Optional<String> findAny = list1.parallelStream().filter(s -> s.startsWith("J")).findAny();
+
+        System.out.println(findFirst.get());
+        System.out.println(findAny.get());
+
+        System.out.println("--------------------------------------");
+
+        //MIN AND MAX
+        Optional<Dish> dMin = DishData.getAll().stream()
+                .min(Comparator.comparing(Dish::getCalories));
+        System.out.println(dMin);
+
+        Optional<Dish> dMax = DishData.getAll().stream().max(Comparator.comparing(Dish::getCalories));
+        System.out.println(dMax);
     }
 }
