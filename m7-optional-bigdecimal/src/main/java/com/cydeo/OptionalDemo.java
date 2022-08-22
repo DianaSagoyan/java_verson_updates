3fec3f10eda821bcf6a2e25494dcd60582e8a6e0
DianaSagoyan <dianasagoyan@yahoo.com>
2022-08-22 12:18:28 -0500

    Practicing Optional

--- m7-optional-bigdecimal/src/main/java/com/cydeo/OptionalDemo.java
+++ m7-optional-bigdecimal/src/main/java/com/cydeo/OptionalDemo.java
@@ -1,4 +1,34 @@
 package com.cydeo;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class OptionalDemo {
+    public static void main(String[] args) {
+        List<Integer> number = Arrays.asList(2, 4, 2, 10, 23);
+
+        //empty() - isPresent()
+        Optional<String> empty = Optional.empty();
+        System.out.println(empty);
+        System.out.println(empty.isPresent());
+
+        System.out.println(Optional.of(number).isPresent());
+
+        System.out.println("------------------------------------");
+
+        //ifPresent
+        Optional<Integer> bigNumber = number.stream().filter(x -> x > 100).findAny();
+        bigNumber.ifPresent(System.out::println);
+
+        System.out.println("------------------------------------");
+
+        //get
+//        System.out.println(bigNumber.get());
+
+        System.out.println("------------------------------------");
+
+        //get
+        System.out.println(bigNumber.orElse(5));
+    }
 }
