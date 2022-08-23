50d50863924f4be2c0283347f18fc3500e95591b
DianaSagoyan <dianasagoyan@yahoo.com>
2022-08-23 15:44:13 -0500

    java14 updates

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/User.java
@@ -0,0 +1,7 @@
+package com.cydeo.java14;
+
+public record User(String name, String address) {
+    //Final that's why no setter
+
+    //we have ALL ARG CONSTRUCTOR, GETTER, TO STRING, EQUALS, HASH CODE
+}
