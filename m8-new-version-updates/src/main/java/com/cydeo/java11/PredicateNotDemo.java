c790376310ee2fdf9b49a08f4e50f7cafea1e06e
DianaSagoyan <dianasagoyan@yahoo.com>
2022-08-23 14:23:54 -0500

    java11 updates

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java11/PredicateNotDemo.java
@@ -0,0 +1,21 @@
+package com.cydeo.java11;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class PredicateNotDemo {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+
+        Predicate<Integer> even = i -> i % 2 == 0;
+        Predicate<Integer> odd = Predicate.not(even); //i -> i % 2 != 0
+
+        List<Integer> evenNumbers = list.stream()
+                .filter(even)
+                .collect(Collectors.toList());
+
+        System.out.println(evenNumbers);
+    }
+}
